a13a3f524fede8b551a5ec6f635faeed6e65ab9a
Shaddowalker-26 <chosentuguchi@gmail.com>
2026-08-23 05:25:58 +0000

    spes: integrate XiaomiParts SELinux policy
    Change-Id: I6d861f89ce4218d3bb8be6fdaf26aba11fe50530

--- device.mk
+++ device.mk
@@ -468,6 +468,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
+
 
 # XiaomiParts
 include packages/apps/XiaomiParts/device.mk
